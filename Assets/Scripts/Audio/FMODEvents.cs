a04b0af6338a1e5e94018fde3f8ba86ea8db0939
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2023-05-10 17:41:35 +0300

    added sound manager and fmod event script

---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Ambience")]
+
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference Music { get; private set; }
+
+    [field: Header("Player SFX")]
+
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events instance in the scene.");
+        }
+        instance = this;
+    }
+}
