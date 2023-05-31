93eb5efa15efdb2ac8abcf94599c9aa76609855c
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2023-05-31 17:34:38 +0300

    level 3

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private float floatSpeed = 4.0f;
+    [SerializeField] private float floatAmount = 0.25f;
+
+    private Vector3 originalPosition;
+    private float offset;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        offset = floatAmount; // Offset equals to float amount to prevent going below original position
+    }
+
+    private void Update()
+    {
+        Float();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(playerTag))
+        {
+            Debug.Log("Collectable collected!");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Float()
+    {
+        float newY = originalPosition.y + offset + Mathf.Sin(Time.time * floatSpeed) * floatAmount;
+        transform.position = new Vector3(originalPosition.x, newY, originalPosition.z);
+    }
+}
