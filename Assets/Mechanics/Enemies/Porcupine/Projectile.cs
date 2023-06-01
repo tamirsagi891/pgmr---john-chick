152f92a8762990689b324bbd90bb845db774b52e
barne <barnemesh@gmail.com>
2023-06-01 18:49:47 +0300

    projectile base

---
+++ Assets/Mechanics/Enemies/Porcupine/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using Avrahamy;
+using Nemesh;
+using UnityEngine;
+
+namespace Mechanics.Enemies.Porcupine
+{
+    [AddComponentMenu("NPC/Attack Controls/Base Projectile")]
+    public class Projectile : OptimizedBehaviour, IPoolable
+    {
+        public ProjectilePool MyPool { get; set; }
+        
+        public void ReleaseSelf()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InitObject()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Mechanics/Enemies/Porcupine/ProjectilePool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BitStrap;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Mechanics.Enemies.Porcupine
+{
+    public class ProjectilePool : MonoBehaviour
+    {
+        [SerializeField]
+        [RequiredReference]
+        private Projectile projectilePrefab;
+        
+        public LinkedPool<Projectile> MyPool { get; set; }
+        private void Awake()
+        {
+            MyPool = new LinkedPool<Projectile>(
+                InitProjectile, 
+                GetProjectile,
+                ReleaseProjectile,
+                DestroyProjectile);
+        }
+
+        private void DestroyProjectile(Projectile projectile)
+        {
+            Destroy(projectile.gameObject);
+        }
+
+        private void ReleaseProjectile(Projectile projectile)
+        {
+            projectile.gameObject.SetActive(false);
+        }
+
+        private Projectile InitProjectile()
+        {
+            var projectile = Instantiate(projectilePrefab, transform);
+            projectile.MyPool = this;
+            return projectile;
+        }
+
+        private void GetProjectile(Projectile projectile)
+        {
+            projectile.gameObject.SetActive(true);
+            projectile.InitObject();
+        }
+        
+        
+    }
+}
