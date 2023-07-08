8f0e92b7254da4f544a3a4889f47e12e161acc55
barne <barnemesh@gmail.com>
2023-07-08 03:06:02 +0300

    Rays updated

--- Assets/Mechanics/UI/Menus/BaseMenuController.cs
+++ Assets/Mechanics/UI/Menus/BaseMenuController.cs
@@ -1,7 +1,9 @@
-﻿using BitStrap;
+﻿using System;
+using BitStrap;
 using Elad.Scripts;
 using Elad.Scripts.Combat;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using Logger = Nemesh.Logger;
 
@@ -18,18 +20,33 @@ namespace Mechanics.UI.Menus
         [SerializeField]
         [RequiredReference]
         protected GameObject firstSelected;
+
+        [Space]
+        [SerializeField]
+        private MenuEvents events;
         
         public virtual void OpenMenu()
         {
-            
             menuUiObject.SetActive(true);
             EventSystem.current.SetSelectedGameObject(firstSelected);
+            events.onOpen.Invoke();
         }
 
         public virtual void CloseMenu()
         {
             menuUiObject.SetActive(false);
+            events.onClose.Invoke();
         }
 
+    }
+
+    [Serializable]
+    public struct MenuEvents
+    {
+        [SerializeField]
+        public UnityEvent onOpen;
+
+        [SerializeField]
+        public UnityEvent onClose;
     }
 }
